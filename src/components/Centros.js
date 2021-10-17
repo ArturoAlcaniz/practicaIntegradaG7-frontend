ba4468912b6007213953685333d5fbea268761c6
beatr <beatr@DESKTOP-GRTSOST>
2021-10-17 13:36:40 +0200

    Modal para añadir los datos del centro

--- src/components/Centros.js
+++ src/components/Centros.js
@@ -1,12 +1,68 @@
-import {React, Component} from "react";
+import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Centros extends Component{
     render() {
         return (
-        	<div>
-				<h3>Hola</h3>
-			</div>
+        	<div class="container-fluid px-4">
+                <h1>Centros de salud</h1>
+                <div class="card mb-4">
+                <div class="card-header">
+                    Centros
+                </div>
+                <div class="card-body">
+                    <div class="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
+                        <div class="dataTable-container">
+                            <div class="container mt-3">
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">Add Centro</button>
+
+                                <div class="modal fade" id="myModal">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">Datos centro</h4>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <h1>HOLA</h1>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <table class="table table-hover">
+                                <thead>
+                                  <th>Nombre del centro</th>
+                                  <th charset="utf-8">Dirección</th>
+                                  <th>Vacunas disponibles</th> 
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
         );
     }
 }
