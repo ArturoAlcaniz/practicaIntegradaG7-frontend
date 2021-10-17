13de710dc07d88c25921eb61fa5d1cde948cc95f
beatr <beatr@DESKTOP-GRTSOST>
2021-10-17 21:33:24 +0200

    Formulario creado

--- src/components/Centros.js
+++ src/components/Centros.js
@@ -6,46 +6,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Centros extends Component{
     render() {
         return (
-        	<div class="container-fluid px-4">
+        	<div className="container-fluid px-4">
                 <h1>Centros de salud</h1>
-                <div class="card mb-4">
-                <div class="card-header">
-                    Centros
-                </div>
-                <div class="card-body">
-                    <div class="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
-                        <div class="dataTable-container">
-                            
-                            <a class="btn btn-success" aria-current="page" href="/Formulario">Add Centro</a>
-                        
-                            <table class="table table-hover">
-                                <thead>
-                                  <th>Nombre del centro</th>
-                                  <th charset="utf-8">Dirección</th>
-                                  <th>Vacunas disponibles</th> 
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                <div className="card mb-4">
+                    <div className="card-header">
+                        Centros
+                    </div>
+                    <div className="card-body">
+                        <div className="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
+                            <div className="dataTable-container">
+
+                                <table className="table table-hover">
+                                    <thead>
+                                        <th>Nombre del centro</th>
+                                        <th>Dirección</th>
+                                        <th>Vacunas disponibles</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
             </div>
+            <a className="btn btn-success" aria-current="page" href="/FormularioCentros">Add Centro</a>
         </div>
 
         );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
-
+import '../src/utils/styles/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './components/App/App';
-import Centros from './components/Centros'
+import Centros from './components/Centros';
+import FormularioCentros from './components/FormularioCentros';
 
 function Routing() {
 	return (
@@ -15,6 +16,7 @@ function Routing() {
 			<App />
 			<Router>
 				<Route path="/Centros" exact component={Centros} />
+				<Route path="/FormularioCentros" exact component={FormularioCentros} />
 			</Router>
 		</div>
 	)
