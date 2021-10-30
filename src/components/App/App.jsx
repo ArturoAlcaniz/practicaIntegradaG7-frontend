559000d1a99f90d7acc3f898f3527a09175049eb
Ismael Martín-Portugués Villegas <32538380+puzzismael@users.noreply.github.com>
2021-10-30 17:10:52 +0200

    Corregido Errores

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,52 +1,58 @@
 import React from 'react';
-import {BrowserRouter as Router, NavLink, Switch, Route}  from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Switch, Route } from "react-router-dom";
 import Centros from '../Centros';
 import FormularioCentros from '../FormularioCentros';
 import Usuarios from '../Usuarios';
 import FormularioUsuarios from '../FormularioUsuarios';
 import Appointment from '../Appointment';
+import Cupos from '../Cupos';
 
-function App () {
-     return (<Router>
-         <div className="App">
-           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-             <div className="container-fluid">
-			    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-			      <span className="navbar-toggler-icon"></span>
-			    </button>
-			    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-			      <ul className="navbar-nav">
-			        <li className="nav-item">
-			          <NavLink className="nav-link" to="/Centros" activeClassName="nav-link active">
-						  Centros
-						</NavLink>
-			        </li>
-					<li className="nav-item">
-			          <NavLink className="nav-link" to="/Usuarios" activeClassName="nav-link active">
-						  Usuarios
-						</NavLink>
-			        </li>
-					<li className="nav-item">
-			          <NavLink className="nav-link" to="/Appointment" activeClassName="nav-link active">
-						  Pedir cita
-						</NavLink>
-			        </li>
-			        <li className="nav-item">
-			          <a className="nav-link active" aria-current="page" href="/Cupos">Cupos</a>
-			        </li>
-			      </ul>
-			    </div>
-			  </div>  
-           </nav>
-		<Switch>
-              <Route exact path='/Centros' exact component={Centros} />
-              <Route path='/Usuarios' exact component={Usuarios} />
-              <Route path='/Appointment' exact component={Appointment} />
-              <Route path='/FormularioCentros' exact component={FormularioCentros} />
-              <Route path='/FormularioUsuarios' exact component={FormularioUsuarios} />
-          </Switch>
-        </div>
-    </Router>);
+
+
+function App() {
+	return (<Router>
+		<div className="App">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<div className="container-fluid">
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarNavDropdown">
+						<ul className="navbar-nav">
+							<li className="nav-item">
+								<NavLink className="nav-link" to="/Centros" activeClassName="nav-link active">
+									Centros
+</NavLink>
+							</li>
+							<li className="nav-item">
+								<NavLink className="nav-link" to="/Usuarios" activeClassName="nav-link active">
+									Usuarios
+</NavLink>
+							</li>
+							<li className="nav-item">
+								<NavLink className="nav-link" to="/Appointment" activeClassName="nav-link active">
+									Pedir cita
+</NavLink>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="/Cupos">Cupos</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<Switch>
+				<Route exact path='/Centros' exact component={Centros} />
+				<Route path='/Usuarios' exact component={Usuarios} />
+				<Route path='/Appointment' exact component={Appointment} />
+				<Route path='/FormularioCentros' exact component={FormularioCentros} />
+				<Route path='/FormularioUsuarios' exact component={FormularioUsuarios} />
+				<Route path='/Cupos' exact component={Cupos} />
+			</Switch>
+		</div>
+	</Router>);
 }
+
+
 
 export default App;
