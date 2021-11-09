8ddbe3cf5340377d83d7edf6ee05e7b8c359c498
Pablo <Pablo.Guerrero@alu.uclm.es>
2021-11-09 12:34:22 +0100

    Lista de Vacunacion arreglado dia por defecto

--- src/components/ListaVacunacion.js
+++ src/components/ListaVacunacion.js
@@ -6,24 +6,18 @@ import env from"react-dotenv";
 export default class ListaVacunacion extends Component {
 	constructor(props) {
 		super(props);
-		let today = new Date(),
-		date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+		let today = new Date();
+		let dia;
+		if (today.getDate()<10) {
+			dia = '0'+today.getDate();
+		}else {
+			dia = today.getDate();
+		}
+		let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + dia;
 		this.state = {
 			usuarios: [],
 			fecha: date
 		}
-	}
-
-	obtenerDatos(thisComponent){
-		async function getUsuarios(){
-				let answer = await fetch(env[process.env.NODE_ENV+'_API_URL']+'/usuarios/obtener', {
-			method: "GET"
-		});
-		
-		let json = await answer.text();
-		thisComponent.setState({usuarios: JSON.parse(json)})
-		}
-		getUsuarios();
 	}
 	
 	vacunar(event) {
@@ -41,7 +35,10 @@ export default class ListaVacunacion extends Component {
 							<div className="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
 								<div className="dataTable-container">
 									<div>Fecha</div>
-									<div className='date'>{this.state.fecha}</div>
+									<div><input type="date" id="start" name="trip-start"
+       									defaultValue={this.state.fecha}
+       									min="2021-11-01" max="2022-01-31"/>
+									</div>
 									<table className="table table-hover">
 										<thead>
 											<tr>
@@ -53,19 +50,6 @@ export default class ListaVacunacion extends Component {
 											</tr>
 										</thead>
 										<tbody>
-											{this.state.usuarios.map((listValue, index) => {
-												return (
-													<tr key={index}>
-														<td>{listValue.dni}</td>
-														<td>{listValue.nombre}</td>
-														<td>{listValue.apellidos}</td>
-														<td>{listValue.segundaDosis ? 'Segunda' : 'Primera'}</td>
-														<td>
-															<button onClick={this.vacunar}>Vacunar</button>
-														</td>
-													</tr>
-												);
-											})}
 										</tbody>
 									</table>
 								</div>
@@ -76,9 +60,5 @@ export default class ListaVacunacion extends Component {
 				</div>
 			</div>
 		);
-	}
-	
-	componentDidMount(){
-		this.obtenerDatos(this);
 	}
 }
