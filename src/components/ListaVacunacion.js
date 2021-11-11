fc945cc3f02e163abfbe21c4331d2745069c8cef
Arturo <arturoguijarro@hotmail.com>
2021-11-11 15:45:14 +0100

    fixed field vacunacion

--- src/components/ListaVacunacion.js
+++ src/components/ListaVacunacion.js
@@ -57,7 +57,7 @@ export default class ListaVacunacion extends Component {
 														<td>{listValue.dni}</td>
 														<td>{listValue.nombre}</td>
 														<td>{listValue.primeraDosis ? "2" : "1"}</td>
-														<VacunarPaciente dataVacunacion={[listValue.dni]} />
+														<VacunarPaciente dataVacunacion={[listValue.email]} />
 													</tr>
 												);
 											})}
@@ -79,7 +79,7 @@ async function VacunarPaciente({dataVacunacion}) {
 	const handleVacunacion = async function() {
 		let answer = await fetch(env[process.env.NODE_ENV+'_API_URL']+'/usuario/vacunar', {
 			method: "POST",
-			body: JSON.stringify({dni: dataVacunacion[0]}),
+			body: JSON.stringify({email: dataVacunacion[0]}),
 			headers: { 
 				'Accept': 'application/json',
 				'Content-Type': 'application/json' 
